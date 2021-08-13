be290bfd9b9d75c88e9cc15e349cd93b78f58e3c
zNaflyy <82005263+zNaflyy@users.noreply.github.com>
2021-08-13 12:12:37 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+
+const headers = {
+	'Content-Type': 'application/json',
+	Accept: 'application/json',
+	'Accept-Encoding': 'gzip, deflate, br',
+	Connection: 'keep-alive',
+	'X-Requested-With': 'Repl.it',
+	Referrer: 'https://repl.it',
+	Origin: 'https://repl.it'
+};
+
+var getData = async function(username) {
+	let info = await fetch('https://repl.it/graphql', {
+		method: 'POST',
+		headers,
+		body: JSON.stringify({
+			query:
+				`{userByUsername(username: "${username}") {karma, firstName, lastName, bio, isVerified, timeCreated, isLoggedIn, organization {name}, subscription {planId}, roles { name }}}`
+		})
+	}).then(res => res.json());
+	console.log(info.data.userByUsername)
+};
+
+//Replace this with the username you want!
+getData('[username]');
